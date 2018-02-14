afe6f3d633f38f1f69d26bac1dd0731532765014
‘Michael <michael.daniels.jr@gmail.com>
2018-02-14 11:47:33 -0700

    chapter 15 exercise push 1

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 var fs = require('fs');
+
+// fs.readFile("challenge1/info.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+//
+// fs.readFile("challenge2/info.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     if (!data.includes("Michael")) {
+//       data += " - Michael";
+//       fs.writeFileSync("challenge2/info.txt", data)
+//       console.log(data);
+//     }
+//   }
+// });
+//
+// fs.rename("challenge3/binfo.txt", "challenge3/info.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
+
+fs.mkdir("challenge4/copyfolder", (err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("challenge4/copyfolder created")
+  }
+});
